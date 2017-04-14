36f5156dd1262c79a3194cc35d785f244af08289
Sadavoya <sadavoya@gmail.com>
2017-04-14 10:49:29 -0400

    Added Interactables to room and examineDict to interactableItems

--- Assets/Scripts/TBA/InteractableItems.cs
+++ Assets/Scripts/TBA/InteractableItems.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractableItems : MonoBehaviour {
+    public Dictionary<string, string> examineDict = new Dictionary<string, string>();
+
     [HideInInspector]
     public List<string> nounsInRoom = new List<string>();
 
@@ -11,11 +13,11 @@ public class InteractableItems : MonoBehaviour {
 
     public string GetObjectsNotInInventory(Room currentRoom, int i)
     {
-        var interactable = currentRoom.Interactables[i];
-        if (!nounsInInventory.Contains(interactable.Noun))
+        var objectInRoom = currentRoom.Interactables[i];
+        if (!nounsInInventory.Contains(objectInRoom.Noun))
         {
-            nounsInRoom.Add(interactable.Noun);
-            return interactable.Description;
+            nounsInRoom.Add(objectInRoom.Noun);
+            return objectInRoom.Description;
         }
         return null;
     }
